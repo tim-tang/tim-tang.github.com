7c81950601b4c1dfd0f33bbeade9818f8827120d
tim.tang <tang.jilong@139.com>
2013-01-31 19:03:33 +0800

    Update jekyll blog

--- assets/themes/mark-reid/js/techtalkin.js
+++ assets/themes/mark-reid/js/techtalkin.js
@@ -128,3 +128,22 @@ var data = false;
 
 })(jQuery);
 
+(function() {
+  $(document).ready(function() {
+    $("a.go_top").click(function() {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 300);
+      return false;
+    });
+    return $(window).bind('scroll resize', function() {
+      var scroll_from_top;
+      scroll_from_top = $(window).scrollTop();
+      if (scroll_from_top >= 1) {
+        return $("a.go_top").show();
+      } else {
+        return $("a.go_top").hide();
+      }
+    });
+  });
+}).call(this);
