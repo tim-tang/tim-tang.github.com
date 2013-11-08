5df2c543e93ec5416c7767d451cb4ba19a81466e
tim.tang <tang.jilong@139.com>
2013-11-08 12:50:20 +0800

    Format grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,104 +1,81 @@
 'use strict';
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        'assets/themes/mark-reid/js/*.js',
-        '!assets/themes/mark-reid/js/scripts.min.js'
-      ]
-    },
-    recess: {
-      dist: {
-        options: {
-          compile: true,
-          compress: true
+    grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: ['Gruntfile.js', 'assets/themes/mark-reid/js/*.js', '!assets/themes/mark-reid/js/scripts.min.js']
         },
-        files: {
-          'assets/themes/mark-reid/css/main.min.css': [
-            'assets/themes/mark-reid/css/*.css'
-          ]
-        }
-      }
-    },
-    uglify: {
-      dist: {
-        files: {
-          'assets/themes/mark-reid/js/main.min.js': [
-            'assets/themes/mark-reid/js/*.js'
-          ]
-        }
-      }
-    },
-    imagemin: {
-      dist: {
-        options: {
-          optimizationLevel: 7,
-          progressive: true
+        recess: {
+            dist: {
+                options: {
+                    compile: true,
+                    compress: true
+                },
+                files: {
+                    'assets/themes/mark-reid/css/main.min.css': ['assets/themes/mark-reid/css/*.css']
+                }
+            }
         },
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: '{,*/}*.{png,jpg,jpeg}',
-          dest: 'images/'
-        }]
-      }
-    },
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: '{,*/}*.svg',
-          dest: 'images/'
-        }]
-      }
-    },
-    watch: {
-      css: {
-        files: [
-          'assets/thems/mark-reid/css/*.css'
-        ],
-        tasks: ['recess']
-      },
-      js: {
-        files: [
-          '<%= jshint.all %>'
-        ],
-        tasks: ['uglify']
-      }
-    },
-    clean: {
-      dist: [
-        'assets/themes/mark-reid/css/main.min.css',
-        'assets/themes/mark-reid/js/scripts.min.js'
-      ]
-    }
-  });
+        uglify: {
+            dist: {
+                files: {
+                    'assets/themes/mark-reid/js/main.min.js': ['assets/themes/mark-reid/js/*.js']
+                }
+            }
+        },
+        imagemin: {
+            dist: {
+                options: {
+                    optimizationLevel: 7,
+                    progressive: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: '{,*/}*.{png,jpg,jpeg}',
+                    dest: 'images/'
+                }]
+            }
+        },
+        svgmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: '{,*/}*.svg',
+                    dest: 'images/'
+                }]
+            }
+        },
+        watch: {
+            css: {
+                files: ['assets/thems/mark-reid/css/*.css'],
+                tasks: ['recess']
+            },
+            js: {
+                files: ['<%= jshint.all %>'],
+                tasks: ['uglify']
+            }
+        },
+        clean: {
+            dist: ['assets/themes/mark-reid/css/main.min.css', 'assets/themes/mark-reid/js/scripts.min.js']
+        }
+    });
 
-  // Load tasks
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-recess');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-svgmin');
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-recess');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-svgmin');
 
-  // Register tasks
-  grunt.registerTask('default', [
-    'clean',
-    'recess',
-    'uglify',
-    'imagemin',
-    'svgmin'
-  ]);
-  grunt.registerTask('dev', [
-    'watch'
-  ]);
+    // Register tasks
+    grunt.registerTask('default', ['clean', 'recess', 'uglify', 'imagemin', 'svgmin']);
+    grunt.registerTask('dev', ['watch']);
 
 };
