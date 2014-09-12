b259367aa2e909be6e02d3ed88ecd58f6260929f
tim.tang <tang.jilong@139.com>
2014-09-12 20:12:31 +0800

    Implement auto deploy.

---
+++ bin/travis_encrypt_key_mac.sh
@@ -0,0 +1,16 @@
+# On a Mac, use this script to generate secure deployment key
+if [[ ! $(which travis) ]]
+then
+  gem install travis
+fi
+
+# To generate secure SSH deploy key for a github repo to be used from Travis
+base64 --break=0 ~/.ssh/id_rsa_linode > ~/.ssh/id_rsa_linode_base64
+ENCRYPTION_FILTER="echo \$(echo \"- secure: \")\$(travis encrypt \"\$FILE='\`cat $FILE\`'\" -r tim-tang/tim-tang.github.com)"
+# If you don't have homebrew please install it from http://brew.sh/
+if [[ ! $(which brew) ]]
+then
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" 
+fi
+brew install coreutils 
+gsplit --bytes=100 --numeric-suffixes --suffix-length=2 --filter="$ENCRYPTION_FILTER" ~/.ssh/id_rsa_linode_base64 id_rsa_
